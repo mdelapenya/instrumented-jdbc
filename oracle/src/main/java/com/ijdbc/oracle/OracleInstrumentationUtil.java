069ad1e544447fcde5c57c305461b308bd9b3455
Manuel de la Peña <manuel.delapenya@liferay.com>
2016-12-05 19:35:51 +0100

    Utility class to make easy oracle instrumentation

---
+++ oracle/src/main/java/com/ijdbc/oracle/OracleInstrumentationUtil.java
@@ -0,0 +1,34 @@
+package com.ijdbc.oracle;
+
+import com.ijdbc.common.Instrumentator;
+import com.ijdbc.common.JDBCInstrumentation;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+/**
+ * @author Manuel de la Peña
+ */
+public class OracleInstrumentationUtil {
+
+	public static void instrument(
+			Connection connection, String actionName, String clientInfo,
+			String moduleName)
+		throws SQLException {
+
+		instrument(
+			connection,
+			new JDBCInstrumentation(actionName, clientInfo, moduleName));
+	}
+
+	public static void instrument(
+			Connection connection, JDBCInstrumentation jdbcInstrumentation)
+		throws SQLException {
+
+		Instrumentator instrumentator = new OracleConnectionInstrumentator(
+			jdbcInstrumentation);
+
+		instrumentator.instrument(connection);
+	}
+
+}
