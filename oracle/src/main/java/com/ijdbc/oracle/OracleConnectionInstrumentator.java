eab9196052826c0d5ae880b58ed5bfc2c6150d6e
Manuel de la Peña <manuel.delapenya@liferay.com>
2016-12-05 07:45:56 +0100

    Implement Oracle instrumentation method

---
+++ oracle/src/main/java/com/ijdbc/oracle/OracleConnectionInstrumentator.java
@@ -0,0 +1,47 @@
+package com.ijdbc.oracle;
+
+import com.ijdbc.common.Instrumentator;
+import com.ijdbc.common.JDBCInstrumentation;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import oracle.jdbc.OracleConnection;
+
+/**
+ * @author mdelapenya
+ */
+public class OracleConnectionInstrumentator implements Instrumentator {
+
+	public OracleConnectionInstrumentator(
+		JDBCInstrumentation defaultJdbcInstrumentation) {
+
+		this.defaultJdbcInstrumentation = defaultJdbcInstrumentation;
+	}
+
+	@Override
+	public void instrument(Connection connection) throws SQLException {
+		instrument(connection, defaultJdbcInstrumentation);
+	}
+
+	@Override
+	public void instrument(
+			Connection connection, JDBCInstrumentation jdbcInstrumentation)
+		throws SQLException {
+
+		String[] metrics =
+			new String[OracleConnection.END_TO_END_STATE_INDEX_MAX];
+
+		metrics[OracleConnection.END_TO_END_CLIENTID_INDEX] =
+			jdbcInstrumentation.getClientInfo();
+		metrics[OracleConnection.END_TO_END_MODULE_INDEX] =
+			jdbcInstrumentation.getModuleName();
+		metrics[OracleConnection.END_TO_END_ACTION_INDEX] =
+			jdbcInstrumentation.getActionName();
+
+		((OracleConnection)connection).setEndToEndMetrics(metrics, (short)0);
+	}
+
+	private JDBCInstrumentation defaultJdbcInstrumentation;
+
+}
